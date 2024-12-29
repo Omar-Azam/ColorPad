f63651fdbab52b2791ab2427c01f17e5ea561c82
Omar-Azam <thisisomarazam@gmail.com>
2024-12-30 03:49:37 +0500

    bug fixes and improved animations

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let input = document.querySelector("input");
 let one = document.querySelector(".one");
 let all = document.querySelector(".all");
 let editor = "close";
+
+// Array
 let colors = [
   "Teal",
   "Violet",
@@ -24,12 +26,13 @@ function createButton(text) {
   button.innerText = text;
   button.addEventListener("click", (e) => {
     document.body.style.backgroundColor = text.toLowerCase();
-    console.log(text);
   });
   strip.appendChild(button);
 }
+
+// This function creates first ten buttons
 colors.forEach((color) => createButton(color));
-window.addEventListener("keydown", (e) => {});
+
 function openAnimation() {
   input.classList.add("input-s-ani");
   input.classList.remove("input-s-ani-close");
@@ -37,7 +40,9 @@ function openAnimation() {
   one.classList.remove("one-ani-close");
   all.classList.add("all-ani");
   all.classList.remove("all-ani-close");
+  animAdd();
 }
+
 function closeAnimation() {
   input.classList.remove("input-s-ani");
   input.classList.add("input-s-ani-close");
@@ -45,18 +50,37 @@ function closeAnimation() {
   one.classList.add("one-ani-close");
   all.classList.remove("all-ani");
   all.classList.add("all-ani-close");
+  animClose();
+}
+
+function animAdd() {
+  let btn = document.querySelectorAll("button");
+  Array.from(btn).forEach((btn) => {
+    btn.classList.add("btn-ani");
+  });
+}
+
+function animClose() {
+  let btn = document.querySelectorAll("button");
+  Array.from(btn).forEach((btn) => {
+    btn.classList.remove("btn-ani");
+  });
 }
 
 // Events
-if (colors.length < 10) {
-  if (input.value == "") {
-  } else if (e.key === "Enter") {
-    colors.push(input.value);
-    input.value = "";
-    strip.innerHTML = "";
-    colors.forEach((color) => createButton(color));
+window.addEventListener("keydown", (e) => {
+  if (colors.length < 10) {
+    if (input.value == "") {
+    } else if (e.key === "Enter") {
+      colors.push(input.value);
+      input.value = "";
+      strip.innerHTML = "";
+      colors.forEach((color) => createButton(color));
+      animAdd();
+    }
   }
-}
+});
+
 edit.addEventListener("click", (e) => {
   if (editor === "open") {
     closeAnimation();
@@ -65,14 +89,17 @@ edit.addEventListener("click", (e) => {
     openAnimation();
     editor = "open";
   }
-  one.addEventListener("click", (e) => {
-    colors.pop();
-    strip.innerHTML = "";
-    colors.forEach((color) => createButton(color));
-  });
-  all.addEventListener("click", (e) => {
-    colors = [];
-    strip.innerHTML = "";
-    colors.forEach((color) => createButton(color));
-  });
+});
+
+one.addEventListener("click", (e) => {
+  colors.pop();
+  strip.innerHTML = "";
+  colors.forEach((color) => createButton(color));
+  animAdd();
+});
+
+all.addEventListener("click", (e) => {
+  colors = [];
+  strip.innerHTML = "";
+  colors.forEach((color) => createButton(color));
 });
